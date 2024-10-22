637c8dae00ef5703035102adc8457cf4a81f6b06
Stiliyana Simeonova <37340283+stiliyana-simeonova@users.noreply.github.com>
2024-10-22 22:21:18 +0100

    break down components

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
+
+const  NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-center">
+                <div className="nav -header">
+                    <img src={logo} className="nav-logo" alt="backroads" />
+                    <button type="button" className="nav-toggle" id="nav-toggle">
+                        <i className="fas fa-bars"></i>
+                    </button>
+                </div>
+                {/* <!-- left this comment on purpose --> */}
+                <ul className="nav-links" id="nav-links">
+                    {pageLinks.map((link) => {
+                        return (
+                            <li key={link.id}>
+                                <a href={link.href} className="nav-link"> {link.text} </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+
+                <ul className="nav-icons">
+                    <li>
+                        <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+                        ><i className="fab fa-facebook"></i
+                        ></a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+                        ><i className="fab fa-twitter"></i
+                        ></a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+                        ><i className="fab fa-squarespace"></i
+                        ></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
